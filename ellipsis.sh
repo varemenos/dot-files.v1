147723f38b0a80a0bc0588d6e49a42f03998081a
Adonis K <aklp08@gmail.com>
2017-11-09 21:14:58 +0200

    indentation fix

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 pkg.link() {
-    fs.link_files common
+	fs.link_files common
 
-    case $(os.platform) in
-        osx)
-            fs.link_files platform/osx
-            ;;
-        linux)
-            fs.link_files platform/linux
-            ;;
-    esac
+	case $(os.platform) in
+		osx)
+			fs.link_files platform/osx
+			;;
+		linux)
+			fs.link_files platform/linux
+			;;
+	esac
+}
+
 }
