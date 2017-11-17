cac55a142f682f0727f177de4cda09f467717776
Adonis K <aklp08@gmail.com>
2017-11-17 18:27:58 +0200

    use suggested depth for powerline installation script

--- installer/powerline-fonts.sh
+++ installer/powerline-fonts.sh
@@ -2,7 +2,7 @@
 
 pushd
 cd /tmp
-git clone https://github.com/powerline/fonts.git powerline-fonts
+git clone https://github.com/powerline/fonts.git powerline-fonts --depth=1
 cd powerline-fonts
 ./install.sh
 rm -rf /tmp/powerline-fonts
