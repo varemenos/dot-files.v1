646cccc6853d0431a3acdd6aaeb930a8502bc893
Adonis K <aklp08@gmail.com>
2017-11-18 17:06:23 +0200

    move nvidia, powerline-fonts and sensors installer files to dot-files/apps

--- installer/powerline-fonts.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-pushd
-cd /tmp
-git clone https://github.com/powerline/fonts.git powerline-fonts --depth=1
-cd powerline-fonts
-./install.sh
-rm -rf /tmp/powerline-fonts
-popd
