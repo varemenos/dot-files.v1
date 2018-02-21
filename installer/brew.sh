9c035e0080c26f6b428c58805d51ff6de6d74386
Adonis K <aklp08@gmail.com>
2018-02-21 15:32:07 +0100

    setup mac os apps

---
+++ installer/brew.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [[ $CURRENT_OS == 'OS X' ]]; then
+	if test ! $(which brew); then
+		echo "Installing brew"
+
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+		# taps
+        brew tap homebrew/core
+        brew tap homebrew/services
+
+        brew tap caskroom/cask
+        brew tap caskroom/fonts
+        brew tap caskroom/versions
+
+        brew tap bramstein/webfonttools
+	else
+		printf "brew is already installed."
+	fi
+else
+	printf "Aborting, OSX specific"
+fi
