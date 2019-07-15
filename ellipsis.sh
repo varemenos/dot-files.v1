03daaaf1542c8dc1d0a8fb3e9bba07cf7e3ec87d
Adonis K <aklp08@gmail.com>
2019-07-15 21:31:34 +0200

    Update ellipsis.sh

--- ellipsis.sh
+++ ellipsis.sh
@@ -18,4 +18,7 @@ pkg.install() {
 	if [ ! -e "$HOME/.zplug/autoload/zplug" ]; then
 		curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 	fi
+	
+	// fix an oh-my-zsh loading issue with file permission issues
+	compaudit | xargs chmod g-w,o-w
 }
