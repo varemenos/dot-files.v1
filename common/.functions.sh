f8a9ac50999912f0646ed35b125f34a3ef8ee9fa
Adonis Kakoulidis <aklp08@gmail.com>
2019-09-03 18:54:12 +0200

    Update .functions.sh

--- common/.functions.sh
+++ common/.functions.sh
@@ -23,20 +23,7 @@ function whodis() {
 	lsof -n -i:$@ | grep LISTEN
 }
 
-if [[ $CURRENT_OS == 'Linux' ]]; then
-	pull-gnome-extensions() {
-		dconf dump /org/gnome/shell/extensions/ > ./.gnome-extensions.conf
-	}
-
-	push-gnome-extensions() {
-		if cat ./.gnome-extensions.conf > /dev/null; then
-			dconf reset -f /org/gnome/shell/extensions/
-			cat ./.gnome-extensions.conf | dconf load /org/gnome/shell/extensions/
-		fi
-	}
-fi
-
 function setworkgit() {
 	git config user.name "Adonis Kakoulidis"
-	git config user.email "adonis.kakoulidis@nentgroup.com"
+	git config user.email "adonis.kakoulidis@vimla.se"
 }
