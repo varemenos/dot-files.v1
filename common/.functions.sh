efb22ea5e0719de49dd62cc433091c8060aa09fc
Adonis Kakoulidis <aklp08@gmail.com>
2020-10-05 13:35:01 +0300

    add issues function

--- common/.functions.sh
+++ common/.functions.sh
@@ -32,3 +32,21 @@ function setworkgit() {
 	git config user.name "Adonis Kakoulidis"
 	git config user.email "adonis.kakoulidis@vimla.se"
 }
+
+# source: https://github.com/jeremyckahn/dotfiles
+#
+# Use FZF to preview Github issues with FZF and open them in the browser.
+#   Requires:
+#     - https://github.com/cli/cli,
+#     - https://github.com/junegunn/fzf
+#     - https://github.com/sharkdp/bat
+function issues () {
+  # Quoting switches between single and double quotes to leverage and avoid
+  # string interpolation as necessary. There is probably a better way to do
+  # this.
+  gh issue view -w $(
+    gh issue list --limit 100 \
+      | fzf --preview 'gh issue view $(echo {}'" | awk '{ print \$1 }') | bat --color=always -l md" \
+      | awk '{ print $1 }'
+  )
+}
